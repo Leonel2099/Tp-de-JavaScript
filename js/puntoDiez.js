397641805ad12d172e95e344ff9a0e086d5eda34
Leonel2099 <ezequielleonel1@gmail.com>
2023-03-01 14:03:09 -0300

    Punto10-resuelto

---
+++ js/puntoDiez.js
@@ -0,0 +1,4 @@
+let numero = parseInt(prompt("Escribe un numero"));
+if(numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0){
+    document.write("Si es divisible por estos unos de estos numero 2,3,5 y 7")
+}
